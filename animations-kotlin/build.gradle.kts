3aa3c2957f7df780d5f949f568ce2b2335fa3486
hogoshi <hogoshi@yandex.ru>
2024-02-03 00:28:05 +0700

    kotlin module fixed

--- animations-kotlin/build.gradle.kts
+++ animations-kotlin/build.gradle.kts
@@ -1,3 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.8.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
